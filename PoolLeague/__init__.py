bb60a72c9de7006c09da14c9763bf21c296e8709
Tom Harris <thomas.harris@smartbear.com>
2023-04-17 22:46:36 +0100

    app factory; stupid test page

---
+++ PoolLeague/__init__.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+
+import random
+from PoolLeague.eight_ball import RESPONSES
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'pool_league.sqlite')
+    )
+
+    if test_config is None:
+        # load the instance config when not testing, if it exists
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/8ball')
+    def eight_ball():
+        return f"Magic eight ball says ... {random.choice(RESPONSES)}"
+    
+    return app
